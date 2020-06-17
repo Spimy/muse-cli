2dac9cccd620d1d00ab5f3c318bec5bc08a67991
Spimy <officialspyme@gmail.com>
2020-06-17 22:23:29 +0400

    Rename import

--- src/templates/typescript/src/index.ts
+++ src/templates/typescript/src/index.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import MuseClient from './lib/client';
+import MuseClient from './lib/MuseClient';
 
 export const client = new MuseClient();
 client.login(process.env.TOKEN!);
