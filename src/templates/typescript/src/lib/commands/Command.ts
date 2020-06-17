1787bd56b63f9530ebe99dadac0c737c41b4ecdf
Spimy <officialspyme@gmail.com>
2020-06-17 18:58:02 +0400

    Load aliases

--- src/templates/typescript/src/lib/commands/Command.ts
+++ src/templates/typescript/src/lib/commands/Command.ts
@@ -5,7 +5,14 @@ import { CommandExecutor } from './CommandExecutor';
 export function Command(commandInfo: CommandInfo) {
 
     return function (target: { new(): CommandExecutor }) {
-        client.$commands.set(commandInfo, target);
+        client.$commands.set(commandInfo.name, { info: commandInfo, executor: new target() });
+
+        if (commandInfo.aliases) {
+            commandInfo.aliases.forEach(alias => {
+                client.$aliases.set(alias, commandInfo.name);
+            });
+        }
+
     }
 
 }
