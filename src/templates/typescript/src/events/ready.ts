b5c7f570dc9cca71f3ee9946dda33e2f27b399df
Spimy <officialspyme@gmail.com>
2020-06-17 22:24:03 +0400

    Add ready event

---
+++ src/templates/typescript/src/events/ready.ts
@@ -0,0 +1,22 @@
+import { client } from '../index';
+import { Event } from '../lib/events/Event';
+import { EventListener } from '../lib/events/EventListener';
+
+@Event('ready')
+class MessageEvent implements EventListener {
+
+    listen = async () => {
+
+        client.user?.setPresence({
+            activity: {
+                name: `with ${client.users.cache.get(client.$config.ownerID)?.username}`,
+                type: 'PLAYING'
+            },
+            status: "dnd"
+        });
+
+        console.log(`${client.user!.tag} is now ready to be commanded.`);
+
+    }
+
+}
