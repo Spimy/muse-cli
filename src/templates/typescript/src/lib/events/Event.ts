a59a7aeceadc8068f3415ac5a598b4645683f91a
Spimy <officialspyme@gmail.com>
2020-06-17 22:25:43 +0400

    Add event decorator

---
+++ src/templates/typescript/src/lib/events/Event.ts
@@ -0,0 +1,12 @@
+import { client } from '../../index';
+import { EventType } from '../types';
+import { EventListener } from './EventListener';
+
+
+export function Event(event: EventType) {
+
+    return function (target: { new(): EventListener }) {
+        client.on(event, new target().listen);
+    }
+
+}
