4e88e17fcfbeba10530be4b72c3d298ce53e32c1
Spimy <officialspyme@gmail.com>
2020-06-17 16:47:41 +0400

    Add command collection

--- src/templates/typescript/src/lib/client.ts
+++ src/templates/typescript/src/lib/client.ts
@@ -1,9 +1,21 @@
-import { Client, ClientOptions } from 'discord.js';
+import { Client, ClientOptions, Collection } from 'discord.js';
+import { CommandExecutor } from './commands/CommandExecutor';
+import { CommandInfo } from './commands/CommandInfo';
 
 export default class MuseClient extends Client {
 
+    private commands: Collection<CommandInfo, { new(): CommandExecutor }> = new Collection();
+
     constructor(options?: ClientOptions) {
         super(options);
+    }
+
+    /**
+     * Getter $commands
+     * @return {Collection<CommandInfo, { new(): CommandExecutor }}
+     */
+    public get $commands(): Collection<CommandInfo, { new(): CommandExecutor }> {
+        return this.commands;
     }
 
 }
