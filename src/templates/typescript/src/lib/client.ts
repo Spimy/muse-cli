4ac7fb7e13193e4cdaaf9490cb20fe6ec4fcb32f
Spimy <officialspyme@gmail.com>
2020-06-17 17:18:47 +0400

    Update login method

--- src/templates/typescript/src/lib/client.ts
+++ src/templates/typescript/src/lib/client.ts
@@ -1,13 +1,21 @@
 import { Client, ClientOptions, Collection } from 'discord.js';
 import { CommandExecutor } from './commands/CommandExecutor';
 import { CommandInfo } from './commands/CommandInfo';
+import { Utils } from './utils';
 
 export default class MuseClient extends Client {
 
     private commands: Collection<CommandInfo, { new(): CommandExecutor }> = new Collection();
+    private utils: Utils = new Utils(this);
 
     constructor(options?: ClientOptions) {
         super(options);
+    }
+
+    async login(token: string) {
+        super.login(token);
+        await this.utils.loadModules('../commands', true);
+        return token;
     }
 
     /**
