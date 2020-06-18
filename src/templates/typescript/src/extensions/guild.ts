50b28216d5f58ed40e897b6d961c503a43856705
Spimy <officialspyme@gmail.com>
2020-06-18 23:39:56 +0400

    Add guild extension

---
+++ src/templates/typescript/src/extensions/guild.ts
@@ -0,0 +1,31 @@
+import { Structures, TextChannel, VoiceChannel, MessageEmbed } from 'discord.js';
+import { Queue } from '../lib/music/Queue';
+import { MusicPlayer } from '../lib/music/MusicPlayer';
+import { defaultQueue } from '../lib/music/DefaultQueue';
+
+
+
+export class MuseGuild extends Structures.get('Guild') {
+
+    private musicPlayer: MusicPlayer = new MusicPlayer();
+
+    /**
+     * Getter queue
+     * @return {Queue}
+     */
+    public get queue(): Queue {
+        return defaultQueue;
+    }
+
+    /**
+     * Getter player
+     * @return {MusicPlayer}
+     */
+    public get player(): MusicPlayer {
+        return this.musicPlayer;
+    }
+
+
+}
+
+Structures.extend('Guild', () => MuseGuild);
