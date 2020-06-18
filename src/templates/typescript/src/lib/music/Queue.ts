c1a954967d2334eab15ce362eda0832648c3cb58
Spimy <officialspyme@gmail.com>
2020-06-18 23:41:33 +0400

    Add queue interface

---
+++ src/templates/typescript/src/lib/music/Queue.ts
@@ -0,0 +1,16 @@
+import { Music } from './Music';
+import { VoiceConnection, TextChannel, VoiceChannel } from 'discord.js';
+
+export interface Queue {
+
+    current?: Music;
+    upcoming: Music[];
+    volume: number;
+    loop: boolean;
+    playing: boolean;
+
+    connection?: VoiceConnection;
+    textChannel?: TextChannel;
+    voiceChannel?: VoiceChannel;
+
+}
