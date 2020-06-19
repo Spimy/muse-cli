508e04f93eea4b85042a199613018fe4fbbbc5bb
Spimy <officialspyme@gmail.com>
2020-06-19 23:45:19 +0400

    Add setter for queue

--- src/templates/typescript/src/extensions/guild.ts
+++ src/templates/typescript/src/extensions/guild.ts
@@ -1,4 +1,4 @@
-import { Structures, TextChannel, VoiceChannel, MessageEmbed } from 'discord.js';
+import { Structures } from 'discord.js';
 import { Queue } from '../lib/music/Queue';
 import { MusicPlayer } from '../lib/music/MusicPlayer';
 import { defaultQueue } from '../lib/music/DefaultQueue';
@@ -6,13 +6,22 @@ import { defaultQueue } from '../lib/music/DefaultQueue';
 export class MuseGuild extends Structures.get('Guild') {
 
     private musicPlayer: MusicPlayer = new MusicPlayer();
+    private defaultQueue = { ...defaultQueue };
 
     /**
      * Getter queue
      * @return {Queue}
      */
     public get queue(): Queue {
-        return defaultQueue;
+        return this.defaultQueue;
+    }
+
+    /**
+     * Setter queue
+     * @param {Queue} queue
+     */
+    public set queue(queue: Queue) {
+        this.defaultQueue = queue;
     }
 
     /**
