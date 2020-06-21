b9ad7a68c4d1233f217ba5171a7fd210e16029ae
Spimy <officialspyme@gmail.com>
2020-06-21 20:52:22 +0400

    Add stop command

---
+++ src/templates/typescript/src/commands/music/stop.ts
@@ -0,0 +1,27 @@
+import { Command } from '../../lib/commands/Command';
+import { CommandExecutor } from '../../lib/commands/CommandExecutor';
+import { Message } from 'discord.js';
+
+@Command({
+    name: 'stop',
+    aliases: ['end', 'leave'],
+    category: 'Music',
+    description:
+        'Tired of musics? I don\'t think anyone would ever say that! But if you truly are then ' +
+        'stop the music playback using this command!'
+})
+default class implements CommandExecutor {
+
+    execute = async (message: Message): Promise<boolean> => {
+
+        const { queue } = message.guild!;
+        if (typeof queue.current === 'undefined' || message.member!.voice.channel !== queue.voiceChannel) return false;
+
+        queue.current = undefined;
+        queue.upcoming = [];
+        queue.connection?.dispatcher.emit('finish');
+        return true;
+
+    }
+
+}
