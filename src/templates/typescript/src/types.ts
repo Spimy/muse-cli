ee53346b0610a8b2b2511023939d3793c33c27d4
Spimy <officialspyme@gmail.com>
2020-06-23 15:59:29 +0400

    Refator types

--- src/templates/typescript/src/types.ts
+++ src/templates/typescript/src/types.ts
@@ -1,5 +1,10 @@
 import { ClientEvents } from 'discord.js';
 import { Music } from './lib/music/Music';
 
-export type EventType = keyof ClientEvents;
-export type Pages = Music[][];
+type EventType = keyof ClientEvents;
+type Pages = Music[][];
+
+export {
+    EventType,
+    Pages
+}
