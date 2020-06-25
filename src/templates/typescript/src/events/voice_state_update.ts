ced77a862c83c193dcb17831dfdc539c1bd1be65
Spimy <officialspyme@gmail.com>
2020-06-25 20:12:03 +0400

    Remove user from votes if left the voice channel

--- src/templates/typescript/src/events/voice_state_update.ts
+++ src/templates/typescript/src/events/voice_state_update.ts
@@ -1,7 +1,7 @@
-import { Event } from '../lib/events/Event';
-import { EventListener } from '../lib/events/EventListener';
 import { VoiceState } from 'discord.js';
 import { client } from '../index';
+import { Event } from '../lib/events/Event';
+import { EventListener } from '../lib/events/EventListener';
 import { defaultQueue } from '../lib/music/DefaultQueue';
 
 @Event('voiceStateUpdate')
@@ -9,17 +9,27 @@ default class implements EventListener {
 
     listen = async (oldState: VoiceState, newState: VoiceState) => {
 
+        const { queue } = oldState.guild;
+
         if (oldState.member?.user.bot && oldState.member.user === client.user) {
 
-            const { queue } = oldState.guild;
-
-            if (typeof newState.channel === 'undefined' || newState.channel === null) {
+            if (newState.channel === null) {
                 queue.textChannel?.send('🎵 Music playback has ended');
                 oldState.guild.queue.upcoming.length = 0;
                 return oldState.guild.queue = { ...defaultQueue };
             }
 
             queue.voiceChannel = newState.channel;
+
+        }
+
+        if (!oldState.member?.user.bot && newState.channel === null) {
+
+            const { current } = queue;
+            if (current?.votes.includes(oldState.member!)) {
+                const index = current.votes.indexOf(oldState.member!);
+                current.votes.splice(index, 1);
+            }
 
         }
 
