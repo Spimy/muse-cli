b950bd392ad8b4453a524dd85b387354ccf6d80b
Spimy <officialspyme@gmail.com>
2020-06-25 17:08:34 +0400

    Update event templates

--- src/components/typescript/event.js
+++ src/components/typescript/event.js
@@ -1,110 +1,204 @@
-const argumentImports = {
-    channelCreate: 'GuildChannel',
-    channelDelete: 'GuildChannel',
-    channelPinsUpdate: 'GuildChannel, Date',
-    channelUpdate: 'GuildChannel',
-    debug: '',
-    emojiCreate: 'GuildEmoji',
-    emojiDelete: 'GuildEmoji',
-    emojiUpdate: 'GuildEmoji',
-    error: '',
-    guildBanAdd: 'Guild, User',
-    guildBanRemove: 'Guild, User',
-    guildCreate: 'Guild',
-    guildDelete: 'Guild',
-    guildIntegrationsUpdate: 'Guild',
-    guildMemberAdd: 'GuildMember',
-    guildMemberRemove: 'GuildMember',
-    guildMembersChunk: 'Collection, Snowflake, GuildMember, Guild',
-    guildMemberSpeaking: 'GuildMember',
-    guildMemberSpeaking: 'GuildMember, Speaking',
-    guildMemberUpdate: 'GuildMember',
-    guildUnavailable: 'Guild',
-    guildUpdate: 'Guild',
-    invalidated: '',
-    inviteCreate: 'Invite',
-    inviteDelete: 'Invite',
-    message: 'Message',
-    messageDelete: 'Message',
-    messageDeleteBulk: 'Collection, Snowflake, Message',
-    messageReactionAdd: 'MessageReaction, User',
-    messageReactionRemove: ' MessageReaction, User',
-    messageReactionRemoveAll: 'Message',
-    messageReactionRemoveEmoji: 'MessageReaction',
-    messageUpdate: 'Message',
-    presenceUpdate: 'Presence',
-    rateLimit: '',
-    ready: '',
-    roleCreate: 'Role',
-    roleDelete: 'Role',
-    roleUpdate: 'Role',
-    shardDisconnect: '',
-    shardError: '',
-    shardReady: '',
-    shardReconnecting: '',
-    shardResume: '',
-    typingStart: 'Channel, User',
-    userUpdate: 'User',
-    voiceStateUpdate: 'VoiceState',
-    warn: '',
-    webhookUpdate: 'TextChannel'
-}
-
-export const eventArguments = {
-    channelCreate: 'channel: GuildChannel',
-    channelDelete: 'channel: GuildChannel',
-    channelPinsUpdate: 'channel: GuildChannel, time: Date',
-    channelUpdate: 'oldChannel: GuildChannel, newChannel: GuildChannel',
-    debug: 'info: string',
-    emojiCreate: 'emoji: GuildEmoji',
-    emojiDelete: 'emoji: GuildEmoji',
-    emojiUpdate: 'oldEmoji: GuildEmoji, newEmoji: GuildEmoji',
-    error: 'error: Error',
-    guildBanAdd: 'guild: Guild, user: User',
-    guildBanRemove: 'guild: Guild, user: User',
-    guildCreate: 'guild: Guild',
-    guildDelete: 'guild: Guild',
-    guildIntegrationsUpdate: 'guild: Guild',
-    guildMemberAdd: 'member: GuildMember',
-    guildMemberRemove: 'member: GuildMember',
-    guildMembersChunk: 'members: Collection<Snowflake, GuildMember>, guild: Guild',
-    guildMemberSpeaking: 'member: GuildMember',
-    guildMemberSpeaking: 'member: GuildMember, speaking: Readonly<Speaking>',
-    guildMemberUpdate: 'oldMember: GuildMember, newMember: GuildMember',
-    guildUnavailable: 'guild: Guild',
-    guildUpdate: 'oldGuild: Guild, newGuild: Guild',
-    invalidated: '',
-    inviteCreate: 'invite: Invite',
-    inviteDelete: 'invite: Invite',
-    message: 'message: Message',
-    messageDelete: 'message: Message',
-    messageDeleteBulk: 'messages: Collection<Snowflake, Message>',
-    messageReactionAdd: 'messageReaction: MessageReaction, user: User',
-    messageReactionRemove: 'messageReaction: MessageReaction, user: User',
-    messageReactionRemoveAll: 'message: Message',
-    messageReactionRemoveEmoji: 'reaction: MessageReaction',
-    messageUpdate: 'oldMessage: Message, newMessage: Message',
-    presenceUpdate: 'oldPresence?: Presence, newPresence: Presence',
-    rateLimit: 'rateLimitInfo: Object',
-    ready: '',
-    roleCreate: 'role: Role',
-    roleDelete: 'role: Role',
-    roleUpdate: 'oldRole: Role, newRole: Role',
-    shardDisconnect: 'event: CloseEvent, id: number',
-    shardError: 'error: Error, shardID: number',
-    shardReady: 'id: number, unavailableGuilds?: Set<string>',
-    shardReconnecting: 'id: number',
-    shardResume: 'id: number, replayedEvents: number',
-    typingStart: 'channel: Channel, user: User',
-    userUpdate: 'oldUser: User, newUser: User',
-    voiceStateUpdate: 'oldState: VoiceState, newState: VoiceState',
-    warn: 'info: string',
-    webhookUpdate: 'channel: TextChannel'
+export const events = {
+    channelCreate: {
+        imports: 'GuildChannel',
+        args: 'channel: GuildChannel'
+    },
+    channelDelete: {
+        imports: 'GuildChannel',
+        args: 'channel: GuildChannel'
+    },
+    channelPinsUpdate: {
+        imports: 'GuildChannel',
+        args: 'channel: GuildChannel, time: Date'
+    },
+    channelUpdate: {
+        imports: 'GuildChannel',
+        args: 'oldChannel: GuildChannel, newChannel: GuildChannel'
+    },
+    debug: {
+        imports: '',
+        args: 'info: string'
+    },
+    emojiCreate: {
+        imports: 'GuildEmoji',
+        args: 'emoji: GuildEmoji'
+    },
+    emojiDelete: {
+        imports: 'GuildEmoji',
+        args: 'emoji: GuildEmoji'
+    },
+    emojiUpdate: {
+        imports: 'GuildEmoji',
+        args: 'oldEmoji: GuildEmoji, newEmoji: GuildEmoji'
+    },
+    error: {
+        imports: '',
+        args: 'error: Error'
+    },
+    guildBanAdd: {
+        imports: 'Guild, User',
+        args: 'guild: Guild, user: User'
+    },
+    guildBanRemove: {
+        imports: 'Guild, User',
+        args: 'guild: Guild, user: User'
+    },
+    guildCreate: {
+        imports: 'Guild',
+        args: 'guild: Guild'
+    },
+    guildDelete: {
+        imports: 'Guild',
+        args: 'guild: Guild'
+    },
+    guildIntegrationsUpdate: {
+        imports: 'Guild',
+        args: 'guild: Guild'
+    },
+    guildMemberAdd: {
+        imports: 'GuildMember',
+        args: 'member: GuildMember'
+    },
+    guildMemberRemove: {
+        imports: 'GuildMember',
+        args: 'member: GuildMember'
+    },
+    guildMembersChunk: {
+        imports: 'Collection, Snowflake, GuildMember, Guild',
+        args: 'members: Collection<Snowflake, GuildMember>, guild: Guild'
+    },
+    guildMemberSpeaking: {
+        imports: 'GuildMember',
+        args: 'member: GuildMember'
+    },
+    guildMemberSpeaking: {
+        imports: 'GuildMember, Speaking',
+        args: 'member: GuildMember, speaking: Readonly<Speaking>'
+    },
+    guildMemberUpdate: {
+        imports: 'GuildMember',
+        args: 'oldMember: GuildMember, newMember: GuildMember'
+    },
+    guildUnavailable: {
+        imports: 'Guild',
+        args: 'guild: Guild'
+    },
+    guildUpdate: {
+        imports: 'Guild',
+        args: 'oldGuild: Guild, newGuild: Guild'
+    },
+    invalidated: {
+        imports: '',
+        args: ''
+    },
+    inviteCreate: {
+        imports: 'Invite',
+        args: 'invite: Invite'
+    },
+    inviteDelete: {
+        imports: 'Invite',
+        args: 'invite: Invite'
+    },
+    message: {
+        imports: 'Message',
+        args: 'message: Message'
+    },
+    messageDelete: {
+        imports: 'Message',
+        args: 'message: Message'
+    },
+    messageDeleteBulk: {
+        imports: 'Collection, Snowflake, Message',
+        args: 'messages: Collection<Snowflake, Message>'
+    },
+    messageReactionAdd: {
+        imports: 'MessageReaction, User',
+        args: 'messageReaction: MessageReaction, user: User'
+    },
+    messageReactionRemove: {
+        imports: 'MessageReaction, User',
+        args: 'messageReaction: MessageReaction, user: User'
+    },
+    messageReactionRemoveAll: {
+        imports: 'Message',
+        args: 'message: Message'
+    },
+    messageReactionRemoveEmoji: {
+        imports: 'MessageReaction',
+        args: 'reaction: MessageReaction'
+    },
+    messageUpdate: {
+        imports: 'Message',
+        args: 'oldMessage: Message, newMessage: Message'
+    },
+    presenceUpdate: {
+        imports: 'Presence',
+        args: 'oldPresence?: Presence, newPresence: Presence'
+    },
+    rateLimit: {
+        imports: '',
+        args: 'rateLimitInfo: Object'
+    },
+    ready: {
+        imports: '',
+        args: ''
+    },
+    roleCreate: {
+        imports: 'Role',
+        args: 'role: Role'
+    },
+    roleDelete: {
+        imports: 'Role',
+        args: 'role: Role'
+    },
+    roleUpdate: {
+        imports: 'Role',
+        args: 'oldRole: Role, newRole: Role'
+    },
+    shardDisconnect: {
+        imports: 'CloseEvent',
+        args: 'event: CloseEvent, id: number'
+    },
+    shardError: {
+        imports: '',
+        args: 'error: Error, shardID: number'
+    },
+    shardReady: {
+        imports: '',
+        args: 'id: number, unavailableGuilds?: Set<string>'
+    },
+    shardReconnecting: {
+        imports: '',
+        args: 'id: number'
+    },
+    shardResume: {
+        imports: '',
+        args: 'id: number, replayedEvents: number'
+    },
+    typingStart: {
+        imports: 'Channel, User',
+        args: 'channel: Channel, user: User'
+    },
+    userUpdate: {
+        imports: 'User',
+        args: 'oldUser: User, newUser: User'
+    },
+    voiceStateUpdate: {
+        imports: 'VoiceState',
+        args: 'oldState: VoiceState, newState: VoiceState'
+    },
+    warn: {
+        imports: '',
+        args: 'info: string'
+    },
+    webhookUpdate: {
+        imports: 'TextChannel',
+        args: 'channel: TextChannel'
+    }
 }
 
 export const eventTemplate = (name, counter) => {
-    const imports = argumentImports[name];
-    const argument = eventArguments[name];
+    const { imports, args } = events[name];
     const command =
         `
 import { client } from '${'../'.repeat(counter)}index';
@@ -115,7 +209,7 @@ ${imports !== '' ? `import { ${imports} } from 'discord.js'` : ''};
 @Event('${name}')
 default class implements EventListener {
 
-    listen = async (${argument}) => {
+    listen = async (${args}) => {
 
     }
 
